23fc23cd2a5633ae459061fba44c435489ef0fb2
fredefl <frederiklassen@gmail.com>
2013-04-10 23:05:48 +0200

    More unit testing.

--- tests/js/test.js
+++ tests/js/test.js
@@ -1,33 +1,73 @@
-
-test( "Non Salmon Length", function() {
+test( "Non Salmon Lengths", function() {
+	// m->x
 	ok( convert( 5000, "m", "km" ).converted == 5, "m->km" );
 	ok( convert( 5, "m", "dm" ).converted == 50, "m->dm" );
 	ok( convert( 5,	"m", "cm" ).converted == 500, "m->cm" );
 	ok( convert( 5,	"m", "mm" ).converted == 5000, "m->mm" );
+
+	// x->m
 	ok( convert( 5,	"km", "m" ).converted == 5000, "km->m" );
 	ok( convert( 5,	"dm", "m" ).converted == 0.5, "dm->m" );
 	ok( convert( 5,	"cm", "m" ).converted == 0.05, "cm->m" );
 	ok( convert( 5,	"mm", "m" ).converted == 0.005, "mm->m" );
+
+	// x->x
+	ok( convert( 5,	"km", "mm" ).converted == 5000000, "km->mm" );
+	ok( convert( 50000 , "mm", "km" ).converted == 0.05, "mm->km" );
+	ok( convert( 5,	"cm", "dm" ).converted == 0.5, "cm->dm" );
+	ok( convert( 5,	"dm", "cm" ).converted == 50, "dm->cm" );
+
+	// TODO: Imperial
 });
 
+test( "Non Salmon Area", function() {
+	// m2->x
+	ok( convert( 5000, "m2", "km2" ).converted == 0.005, "m2->km2" );
+	ok( convert( 5, "m2", "dm2" ).converted == 500, "m2->dm2" );
+	ok( convert( 5,	"m2", "cm2" ).converted == 50000, "m2->cm2" );
+	ok( convert( 5,	"m2", "mm2" ).converted == 5000000, "m2->mm2" );
 
-/*
-var obj;
-obj = convert(5, "km", "m");
-console.log("5 km to m: " + obj.converted + " " + obj.outUnit);
+	// x->m2
+	ok( convert( 5, "km2", "m2" ).converted == 5000000, "km2->m2" );
+	ok( convert( 5000, "dm2", "m2" ).converted == 50, "dm2->m2" );
+	ok( convert( 5000,	"cm2", "m2" ).converted == 0.5, "cm2->m2" );
+	ok( convert( 5000,	"mm2", "m2" ).converted == 0.005, "mm2->m2" );
 
-obj = convert(500, "cm", "m");
-console.log("500 cm to m : " + obj.converted + " " + obj.outUnit);
+	// x->x
+	ok( convert( 5,	"km2", "mm2" ).converted == 5000000000000, "km2->mm2" );
+	ok( convert( 5000000000000 , "mm2", "km2" ).converted == 5, "mm2->km2" );
+	ok( convert( 5000,	"cm2", "dm2" ).converted == 50, "cm2->dm2" );
+	ok( convert( 5,	"dm2", "cm2" ).converted == 500, "dm2->cm2" );
+});
 
-obj = convert(500, "millimeters", "m");
-console.log("500 millimeters to m : " + obj.converted + " " + obj.outUnit);
+test( "Non Salmon Volume", function() {
+	// m3->x
+	ok( convert( 5000, "m3", "km3" ).converted == 0.000005, "m3->km3" );
+	ok( convert( 5, "m3", "dm3" ).converted == 5000, "m3->dm3" );
+	ok( convert( 5,	"m3", "cm3" ).converted == 5000000, "m3->cm3" );
+	ok( convert( 5,	"m3", "mm3" ).converted == 5000000000, "m3->mm3" );
 
-obj = convert(500, "millimeters", "meters");
-console.log("500 millimeters to meters: " + obj.converted + " " + obj.outUnit);
+	// x->m3
+	ok( convert( 5, "km3", "m3" ).converted == 5000000000, "km3->m3" );
+	ok( convert( 5000, "dm3", "m3" ).converted == 5, "dm3->m3" );
+	ok( convert( 5000,	"cm3", "m3" ).converted == 0.005, "cm3->m3" );
+	ok( convert( 5000,	"mm3", "m3" ).converted == 0.000005, "mm3->m3" );
 
-obj = convert(500, "millimeters", "centimeters");
-console.log("500 millimeters to centimeters: " + obj.converted + " " + obj.outUnit);
+	// x->x
+	ok( convert( 5,	"km3", "mm3" ).converted == 5000000000000000000, "km3->mm3" );
+	ok( convert( 5000000000000000000 , "mm3", "km3" ).converted == 5, "mm3->km3" );
+	ok( convert( 5000,	"cm3", "dm3" ).converted == 5, "cm3->dm3" );
+	ok( convert( 5,	"dm3", "cm3" ).converted == 5000, "dm3->cm3" );
 
-obj = convert(1, "m", "sm");
-console.log("1 meter to salmon meter: " + obj.converted + " " + obj.outUnit);
-*/
+	// TODO: Liters
+});
+
+test( "Non Salmon Temperature", function() {
+	ok( convert( 20, "c", "k" ).converted == 293.15, "c->k" );
+	ok( convert( 0, "k", "c" ).converted == -273.15, "k->c" );
+	ok( convert( 32, "f", "c" ).converted == 0, "f->c" );
+});
+
+test( "Salmon Temperature", function() {
+	ok( convert( 12, "c", "st" ).converted == 1, "c->st" );
+});
