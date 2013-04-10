fe920522862eacd08524db58186dd672f67ce9ac
fredefl <frederiklassen@gmail.com>
2013-04-10 20:18:42 +0200

    Ready for primetime.

--- js/converterv2.js
+++ js/converterv2.js
@@ -1,3 +1,47 @@
+var paths = {
+};
+
+var conversionsLength = conversions.length;
+for (var i = 0; i <= conversionsLength - 1; i++) {
+	var conversion = conversions[i];
+	var unit = conversion.unit;
+	
+	var conversionLength = conversion.conversions.length;
+	for (var j = 0; j <= conversionLength - 1; j++) {
+		var singleConversion = conversion.conversions[j];
+		var steps = [unit, singleConversion.unit];
+		var calc = [singleConversion.calc];
+		addToPath(combine(unit, singleConversion.unit), steps, calc);
+		
+		checkForRoute(unit, singleConversion.unit, steps, calc);
+	}
+}
+
+console.log("Paths", paths);
+
+function convert (inUnit, outUnit, value) {
+	if (typeof acronyms[inUnit] !== "undefined") {
+		inUnit = acronyms[inUnit];
+	}
+	if (typeof acronyms[outUnit] !== "undefined") {
+		inUnit = acronyms[outUnit];
+	}
+
+	var combinedUnits = combine(inUnit, outUnit);
+	if (typeof paths[combinedUnits] === "undefined")
+		return false;
+
+	var calc = paths[combinedUnits].calc;
+	var calcLength = calc.length;
+	var x = value;
+
+	for (var i = 0; i <= calcLength - 1; i++) {
+		x = eval(calc[i]);
+	}
+	console.log(calc);
+	return x;
+}
+
 function combine (v1, v2) {
 	return v1 + "_" + v2;
 }
@@ -30,7 +74,7 @@ function checkForRoute (startUnit, currentUnit, steps, calc) {
 }
 
 function addToPath (units, steps, calc) {
-	if (typeof paths[units] == "undefined") {
+	if (typeof paths[units] === "undefined") {
 		paths[units] = {steps: steps, calc: calc};
 	} else {
 		if (steps.length < paths[units].steps) {
--- js/values.js
+++ js/values.js
@@ -30,3 +30,18 @@ var conversions = [
 		{unit: 	"sm", calc: "x / 1000"}
 	]}
 ];
+
+var acronyms = {
+	"milimeter" 	: "mm",
+	"milimeters" 	: "mm",
+	"centimeter" 	: "cm",
+	"centimeters" 	: "cm",
+	"meter" 		: "m",
+	"meters" 		: "m",
+	"kilometer" 	: "km",
+	"kilometers" 	: "km",
+	"salmon meter" 	: "sm",
+	"salmonmeter" 	: "sm",
+	"salmon meters" : "sm",
+	"salmonmeters" 	: "sm"
+}
