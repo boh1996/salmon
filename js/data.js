da0748205cb48a7c00f9f4ac9100d76a28c0a22e
fredefl <frederiklassen@gmail.com>
2013-04-10 21:09:01 +0200

    Added more units.

--- js/data.js
+++ js/data.js
@@ -1,9 +1,12 @@
 var conversions = [
+	// Length
 	{unit: "m", conversions: [
 		{unit: 	"km", calc: "x / 1000"},
 		{unit: 	"dm", calc: "x * 10"},
 		{unit: 	"cm", calc: "x * 100"},
 		{unit: 	"mm", calc: "x * 1000"},
+		{unit: 	"in", calc: "x * 0.0254"},
+		{unit: 	"ft", calc: "x * 0.3048"},
 		{unit: 	"sm", calc: "x / 0.71"}
 	]},
 	{unit: "km", conversions: [
@@ -17,6 +20,12 @@ var conversions = [
 	]},
 	{unit: "mm", conversions: [
 		{unit: 	"m", calc: "x / 1000"}
+	]},
+	{unit: "in", conversions: [
+		{unit: 	"m", calc: "x / 0.0254"}
+	]},
+	{unit: "ft", conversions: [
+		{unit: 	"m", calc: "x / 0.3048"}
 	]},
 	{unit: "sm", conversions: [
 		{unit: 	"m", calc: "x * 0.71"},
@@ -32,7 +41,43 @@ var conversions = [
 	]},
 	{unit: "msm", conversions: [
 		{unit: 	"sm", calc: "x / 1000"}
-	]}
+	]},
+
+	// Area
+	{unit: "m2", conversions: [
+		{unit: 	"sm2", calc: "x / 0.71 / 0.71"}
+	]},
+	{unit: "sm2", conversions: [
+		{unit: 	"m2", calc: "x * 0.71 * 0.71"}
+	]},
+
+	// Volume
+	{unit: "m3", conversions: [
+		{unit: 	"km3", calc: "x / 1000 / 1000 / 1000"},
+		{unit: 	"dm3", calc: "x * 10 * 10 * 10"},
+		{unit: 	"cm3", calc: "x * 100 * 100 * 100"},
+		{unit: 	"mm3", calc: "x * 1000 * 1000 * 1000"},
+		{unit: 	"l", calc: "x * 1000"},
+		{unit: 	"sm3", calc: "x / 0.71 / 0.71 / 0.71"}
+	]},
+	{unit: "km3", conversions: [
+		{unit: 	"m3", calc: "x * 1000 * 1000 * 1000"}
+	]},
+	{unit: "dm3", conversions: [
+		{unit: 	"m3", calc: "x / 10 / 10 / 10"}
+	]},
+	{unit: "cm3", conversions: [
+		{unit: 	"m3", calc: "x / 100 / 100 / 100"}
+	]},
+	{unit: "mm3", conversions: [
+		{unit: 	"m3", calc: "x / 1000 / 1000 / 1000"}
+	]},
+	{unit: "l", conversions: [
+		{unit: 	"m3", calc: "x / 1000"}
+	]},
+	{unit: "sm3", conversions: [
+		{unit: 	"m3", calc: "x * 0.71 * 0.71 * 0.71"}
+	]},
 ];
 
 var acronyms = {
